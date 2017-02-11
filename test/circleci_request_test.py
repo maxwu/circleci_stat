99aadf66ffb6183fdced0e128746425af689efb3
maxwu <maxwunj@gmail.com>
2017-02-11 16:31:17 +1300

    Update unittest cases for artifact API

--- test/circleci_request_test.py
+++ test/circleci_request_test.py
@@ -12,18 +12,20 @@ class CircleCiReqTest(unittest.TestCase):
         builds = CircleCiReq.get_recent_30builds(token=config.get_cfg_token(), vcs='github', username='maxwu', project='cucumber-java-toy')
         self.assertEqual(30, len(list(builds)))
 
-
     #@unittest.skip("temporarily disabled, test one single artifact list instead")
     def test_30artifacts(self):
         builds = CircleCiReq.get_recent_30artifacts(token=config.get_cfg_token(), vcs='github', username='maxwu', project='cucumber-java-toy')
-        print "numbers are {0}".format(builds)
         self.assertEqual(30, len(list(builds)))
-
 
     def test_artifacts80(self):
         artifacts = CircleCiReq.get_artifacts(token=config.get_cfg_token(), vcs='github', username='maxwu', project='cucumber-java-toy', build_num=80)
+        count = 0
         for artifact in artifacts:
             print 'XML artifact: {}'.format(artifact)
+            self.assertTrue(artifact.endswith('.xml'), 'all artifacts of build 80 are XML files')
+            count += 1
+        self.assertEqual(4, count, 'build 80 shall have 4 artifacts')
+
 
 if __name__ == '__main__':
     unittest.main()
